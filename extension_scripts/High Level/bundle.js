0115615c36bfa8d880834c15307c7363a1fe8525
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-01-29 15:31:55 +1000

    add: custom build script added to xcode

--- extension_scripts/High Level/bundle.js
+++ extension_scripts/High Level/bundle.js
@@ -1,2 +1,4 @@
 "use strict";
 console.log("Dynamic bundle injected!");
+// Change background color of body
+document.body.style.backgroundColor = "red";
--- extension_scripts/Resources/content.js
+++ extension_scripts/Resources/content.js
@@ -2,11 +2,18 @@
 console.log("Static content script injected");
 // Inject firebase CDN script into head
 const dynamicScript = document.createElement('script');
-dynamicScript.src = "https://www.gstatic.com/firebasejs/8.6.8/firebase-app.js";
-// firebaseScript.type = "text/javascript";
-// set script type to 'module'
-dynamicScript.type = "module";
-document.head.appendChild(dynamicScript);
+dynamicScript.type = "text/javascript";
+const link = "https://raw.githubusercontent.com/ActuallyHappening/MySchoolbox/master/extension_scripts/High%20Level/bundle.js";
+const content = fetch(link).then((response) => {
+    return response.text();
+}).then((text) => {
+    console.log("Dynamic script found:", text);
+    dynamicScript.innerHTML = text;
+    document.head.appendChild(dynamicScript);
+    return text;
+}).catch((error) => {
+    console.error("Dynamic script not found:", error);
+});
 /*
 import firebase from 'firebase';
 import firebaseui from 'firebaseui';
