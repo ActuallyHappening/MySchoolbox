5f69d97580e735ef560b2c9d4392fc62817b201e
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-01-29 20:45:17 +1000

    devops(automated): Building & Bundling ...

--- extension_scripts/High Level/Build/bundled.js
+++ extension_scripts/High Level/Build/bundled.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var version$3 = "1.0.5";
+    var version$3 = "1.0.6";
 
     /**
      * @license
--- extension_scripts/Resources/content.js
+++ extension_scripts/Resources/content.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var version = "1.0.5";
+    var version = "1.0.6";
 
     const patch = version.split(".")[2];
     console.log("Static content script injected, patch:", patch, "version:", version);
