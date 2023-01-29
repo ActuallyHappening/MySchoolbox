103712f0f2d7f70cb17634543df1e1f85fe1cd6b
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-01-29 20:08:01 +1000

    feat: Automation now works well!

--- extension_scripts/High Level/build.sh
+++ extension_scripts/High Level/build.sh
@@ -11,7 +11,10 @@ rollup -c rollup.config.mjs
 # echo "Copying content.js & bundled.js into ../Resources"
 # cp ./Build/content.js ../Resources/content.js
 
-# Commit bundled.js
+# Commit auto-generated files
 git add ./Build/bundled.js
+git add ./Build/content.js
+git add package.json
+git add ../Resources/content.js
 git commit -m "devops(automated): Commiting built files Build/bundled.js"
 git push
