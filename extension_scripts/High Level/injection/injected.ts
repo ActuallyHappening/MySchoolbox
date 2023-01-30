e9dff759c6addbf3140bc085e4ca21bc755332ed
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-01-31 07:53:56 +1000

    todo: add sign in ui

--- extension_scripts/High Level/injection/injected.ts
+++ extension_scripts/High Level/injection/injected.ts
@@ -1,6 +1,7 @@
 import { mainDiv } from "../parser/parse"
 export const signInBtn = document.createElement('button')
 signInBtn.innerText = "Sign in with Google"
+// export const signOutBtn = // TODO
 
 // Add btn as first child of main div
 mainDiv.insertBefore(signInBtn, mainDiv.firstChild)
