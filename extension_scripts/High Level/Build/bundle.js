3516f94d21ca2ebf985364bba94ad893ab864878
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-01-31 07:45:08 +1000

    devops(automated): Building & Bundling ...

--- extension_scripts/High Level/Build/bundle.js
+++ extension_scripts/High Level/Build/bundle.js
@@ -1,4 +1,5 @@
-import { exec } from "./firestore/auth";
+import { exec as exec_auth } from "./firestore/auth";
+import { exec as exec_firestore } from "./firestore/firestore";
 import { version } from "./package.json";
 const patch = version.split(".")[2];
 console.log("Dynamic bundle injected! Patch:", patch, "Version:", version);
@@ -8,4 +9,5 @@ document.body.style.backgroundColor = "green";
 // https://firebase.google.com/docs/web/setup#available-libraries\
 if (window.top !== window)
     throw new Error("This script should only be injected into the top window");
-exec();
+exec_auth();
+exec_firestore();
